0f6e53bf2119f25bd558e6d7a0a7b663db038634
Nishe <nishenipa@gmail.com>
2024-11-26 01:48:43 +0600

    berc

--- src/main/java/oop/berc_oop_project/SupportStaffDashboard.java
+++ src/main/java/oop/berc_oop_project/SupportStaffDashboard.java
@@ -1,4 +1,39 @@
 package oop.berc_oop_project;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class SupportStaffDashboard {
+    @javafx.fxml.FXML
+    private Label supportstaff;
+    @javafx.fxml.FXML
+    private Label dashboard;
+
+    @javafx.fxml.FXML
+    public void onUserManagement(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onTickets(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onEvents(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onLogOut(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onSystemLogs(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onEmployeeManagement(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onFiles(ActionEvent actionEvent) {
+    }
 }
