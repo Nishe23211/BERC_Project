6c570cec30699758e3a89a546b145fa64addf5b4
Sayma <saymaakter4623@gmail.com>
2024-11-26 10:28:06 +0600

    sayma

--- src/main/java/oop/berc_oop_project/SystemAdministrator2.java
+++ src/main/java/oop/berc_oop_project/SystemAdministrator2.java
@@ -1,4 +1,22 @@
 package oop.berc_oop_project;
 
 public class SystemAdministrator2 {
+    @javafx.fxml.FXML
+    private TextField usernameField;
+    @javafx.fxml.FXML
+    private TableColumn usernameCol;
+    @javafx.fxml.FXML
+    private ComboBox roleComboBox;
+    @javafx.fxml.FXML
+    private TextField passwordField;
+    @javafx.fxml.FXML
+    private TableColumn roleCol;
+    @javafx.fxml.FXML
+    private TableView userroleTable;
+    @javafx.fxml.FXML
+    private TableColumn passwordCol;
+
+    @javafx.fxml.FXML
+    public void onAddButtonClick(ActionEvent actionEvent) {
+    }
 }
