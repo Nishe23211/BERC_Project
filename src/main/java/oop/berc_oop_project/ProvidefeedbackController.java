f23b19ab877f5101cbb9de4e99a17863490511a1
kaziepthi <kaziismatepthi@gmail.com>
2024-11-26 20:33:32 +0600

    first commit >adding fxml and their controler xlass

--- src/main/java/oop/berc_oop_project/ProvidefeedbackController.java
+++ src/main/java/oop/berc_oop_project/ProvidefeedbackController.java
@@ -3,5 +3,21 @@ package oop.berc_oop_project;
 public class ProvidefeedbackController
 {
     @javafx.fxml.FXML
+    private TextField feedbackTF1;
+    @javafx.fxml.FXML
+    private ComboBox categoryCB;
+    @javafx.fxml.FXML
+    private TextField issueTF;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void onsubmitButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onprovideButtonClick(ActionEvent actionEvent) {
+    }
+}
