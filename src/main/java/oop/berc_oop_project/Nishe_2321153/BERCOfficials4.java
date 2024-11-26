d948f9d4504fda9eeebceb7765c576857c8abf1c
Nishe <nishenipa@gmail.com>
2024-11-26 23:00:49 +0600

    berc

--- src/main/java/oop/berc_oop_project/Nishe_2321153/BERCOfficials4.java
+++ src/main/java/oop/berc_oop_project/Nishe_2321153/BERCOfficials4.java
@@ -1,4 +1,34 @@
 package oop.berc_oop_project.Nishe_2321153;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class BERCOfficials4 {
+    @javafx.fxml.FXML
+    private TableColumn filenamecolumn;
+    @javafx.fxml.FXML
+    private TextField caseidfield;
+    @javafx.fxml.FXML
+    private TableColumn fileidcolumn;
+    @javafx.fxml.FXML
+    private DatePicker dofile;
+    @javafx.fxml.FXML
+    private TableView filetable;
+    @javafx.fxml.FXML
+    private TableColumn datecolumn;
+
+    @javafx.fxml.FXML
+    public void onDownload(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onSearch(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onOpenFile(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/oop/berc_oop_project/Nishe_2321153/BERCOfficials8.java
+++ src/main/java/oop/berc_oop_project/Nishe_2321153/BERCOfficials8.java
@@ -1,4 +1,45 @@
 package oop.berc_oop_project.Nishe_2321153;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class BERCOfficials8 {
+    @javafx.fxml.FXML
+    private TextArea descriptionarea;
+    @javafx.fxml.FXML
+    private TableColumn<BERCOfficials8, Integer> topicidcolumn;
+    @javafx.fxml.FXML
+    private Label discussionboard;
+    @javafx.fxml.FXML
+    private TableView<BERCOfficials8> discussiontable;
+    @javafx.fxml.FXML
+    private TextField titlefield;
+    @javafx.fxml.FXML
+    private TextArea guidlinesarea;
+    @javafx.fxml.FXML
+    private TableColumn<BERCOfficials8, String> titlecolumn;
+    @javafx.fxml.FXML
+    private Label setupnewdiscussionboard;
+    @javafx.fxml.FXML
+    private TableColumn<BERCOfficials8, String> commentcolumn;
+
+    @javafx.fxml.FXML
+    public void onCreateBoard(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onCancel(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void onreviewComment(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onPostTopic(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onReviewComment(ActionEvent actionEvent) {
+    }
 }
