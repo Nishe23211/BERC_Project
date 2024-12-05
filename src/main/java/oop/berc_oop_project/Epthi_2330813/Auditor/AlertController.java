00ceafa2367391b08e44bb64595dd05f996f57d6
kaziepthi <kaziismatepthi@gmail.com>
2024-12-05 20:21:34 +0600

    add  type alert   controller class.

--- src/main/java/oop/berc_oop_project/Epthi_2330813/Auditor/AlertController.java
+++ src/main/java/oop/berc_oop_project/Epthi_2330813/Auditor/AlertController.java
@@ -1,18 +1,19 @@
 package oop.berc_oop_project.Epthi_2330813.Auditor;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class AlertController {
 
     @FXML
-    private TableView<?> alertTV;
+    private TableView<Alert> alertTV;
 
     @FXML
-    private TableColumn<?, ?> issueCol;
+    private TableColumn<Alert,String > issueCol;
 
     @FXML
-    private TableColumn<?, ?> nameCol;
+    private TableColumn<Alert, String> nameCol;
 
 }
