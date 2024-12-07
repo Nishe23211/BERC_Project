29c8ba8a97f8ae4d11e125b1ed371cdc3675e236
Sayma <saymaakter4623@gmail.com>
2024-12-07 23:12:25 +0600

    system administrator goal8 complete

--- src/main/java/oop/berc_oop_project/Sayma_2320543/SystemAdministrator8.java
+++ src/main/java/oop/berc_oop_project/Sayma_2320543/SystemAdministrator8.java
@@ -1,10 +1,12 @@
 package oop.berc_oop_project.Sayma_2320543;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class SystemAdministrator8 {
 
@@ -12,16 +14,29 @@ public class SystemAdministrator8 {
     @javafx.fxml.FXML
     private ComboBox<String> typeofinformationComboBox;
     @javafx.fxml.FXML
-    private TableColumn<SystemAdministrator8,String> updateinformationCol;
+    private TableColumn<SystemAdministratorU8,String> updateinformationCol;
     @javafx.fxml.FXML
-    private TableView<SystemAdministrator8> informationTable;
+    private TableView<SystemAdministratorU8> informationTable;
     @javafx.fxml.FXML
-    private TableColumn<SystemAdministrator8,String> typeofinformationCol;
+    private TableColumn<SystemAdministratorU8,String> typeofinformationCol;
     @javafx.fxml.FXML
     private TextField updateinformationField;
+
+    @FXML
+    public void initialize() {
+        typeofinformationComboBox.getItems().addAll("Tariffs","Reports","Notices");
+
+        typeofinformationCol.setCellValueFactory(new PropertyValueFactory<>("typeofinformation"));
+        updateinformationCol.setCellValueFactory(new PropertyValueFactory<>("updateinformation"));
+
+    }
 
 
     @javafx.fxml.FXML
     public void onUpdateButtonClick(ActionEvent actionEvent) {
+        String typeofinformation = typeofinformationComboBox.getSelectionModel().getSelectedItem();
+        String updateinformation = updateinformationField.getText();
+        SystemAdministratorU8 s = new SystemAdministratorU8(typeofinformation,updateinformation);
+        informationTable.getItems().add(s);
     }
 }
