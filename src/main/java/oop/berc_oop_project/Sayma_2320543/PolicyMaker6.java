0c351886997a129b8651e6960e397784657f6e87
Sayma <saymaakter4623@gmail.com>
2024-12-11 23:55:15 +0600

    again create PolicyMaker6 controller class and update scene

--- src/main/java/oop/berc_oop_project/Sayma_2320543/PolicyMaker6.java
+++ src/main/java/oop/berc_oop_project/Sayma_2320543/PolicyMaker6.java
@@ -1,20 +1,19 @@
 package oop.berc_oop_project.Sayma_2320543;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class PolicyMaker6 {
     @javafx.fxml.FXML
     private TextField usernameField;
     @javafx.fxml.FXML
+    private TableColumn<PolicyMaker6,String> devicenameCol;
+    @javafx.fxml.FXML
     private TableColumn<PolicyMaker6,String> usernameCol;
     @javafx.fxml.FXML
     private ComboBox<String> devicenameComboBox;
     @javafx.fxml.FXML
-    private TableColumn<PolicyMaker6,String> deviceCol;
+    private Label DisplayText;
     @javafx.fxml.FXML
     private TextField passwordField;
     @javafx.fxml.FXML
@@ -26,4 +25,7 @@ public class PolicyMaker6 {
     public void onAddButtonClick(ActionEvent actionEvent) {
     }
 
+    @javafx.fxml.FXML
+    public void onRecongnizeButtonClick(ActionEvent actionEvent) {
+    }
 }
