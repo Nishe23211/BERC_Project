91967ce9f89c2896afdf3971c50eca2bf063912d
Nishe <nishenipa@gmail.com>
2024-12-12 00:48:18 +0600

    Add supportstaff8A scene and change the SupportStaff8 scene at a time, Complete SupportStaff8 and SupportStaff8A or goal 8

--- src/main/java/oop/berc_oop_project/Nishe_2321153/SupportStaff8.java
+++ src/main/java/oop/berc_oop_project/Nishe_2321153/SupportStaff8.java
@@ -1,39 +1,79 @@
 package oop.berc_oop_project.Nishe_2321153;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import oop.berc_oop_project.HelloApplication;
+import oop.berc_oop_project.Sayma_2320543.User1;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class SupportStaff8 {
     @javafx.fxml.FXML
     private Label usermanagement;
     @javafx.fxml.FXML
-    private TableView<SupportStaff8> usertable;
+    private TableView<UserManagement> usertable;
     @javafx.fxml.FXML
-    private TableColumn<SupportStaff8, String> usernamecolumn;
+    private TableColumn<UserManagement, String> usernamecolumn;
     @javafx.fxml.FXML
-    private TableColumn<SupportStaff8, String> userrolecolumn;
-    @javafx.fxml.FXML
-    private Label departmentaccess;
+    private TableColumn<UserManagement, String> userrolecolumn;
     @javafx.fxml.FXML
     private TextField usernamefield;
     @javafx.fxml.FXML
-    private Label managepermissions;
+    private Label userText;
     @javafx.fxml.FXML
-    private CheckBox salesbox;
-    @javafx.fxml.FXML
-    private CheckBox restrict;
-    @javafx.fxml.FXML
-    private CheckBox itbox;
-    @javafx.fxml.FXML
-    private Label username;
-    @javafx.fxml.FXML
-    private CheckBox hrbox;
+    private ComboBox<String> roleCB;
+    private ArrayList<UserManagement> management = new ArrayList<>();
+
 
     @javafx.fxml.FXML
-    public void onEdit(ActionEvent actionEvent) {
+    public void onEdit(ActionEvent actionEvent) throws IOException {
+        UserManagement selectedUser = usertable.getSelectionModel().getSelectedItem();
+
+        if (selectedUser == null) {
+            userText.setText("Error: No user selected. Please select a user to edit.");
+            return;
+        }
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("SupportStaff8.a.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) (((Node) actionEvent.getSource()).getScene().getWindow());
+        stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void onSave(ActionEvent actionEvent) {
+    public void onAdd(ActionEvent actionEvent) {
+        String username = usernamefield.getText();
+        String userrole = roleCB.getSelectionModel().getSelectedItem();
+        UserManagement u = new UserManagement(username, userrole);
+        usertable.getItems().add(u);
+
+        usernamefield.clear();
+        roleCB.setValue("");
+
+    }
+
+    @FXML
+    public void initialize() {
+        roleCB.getItems().addAll("Admin", "Editor", "Viewer");
+
+        usernamecolumn.setCellValueFactory(new PropertyValueFactory<>("username"));
+        userrolecolumn.setCellValueFactory(new PropertyValueFactory<>("userrole"));
+
+    }
+
+    public ArrayList<UserManagement> getManagement() {
+        return management;
+    }
+
+    public void setManagement(ArrayList<UserManagement> management) {
+        this.management = management;
     }
 }
--- src/main/java/oop/berc_oop_project/Nishe_2321153/UserManagement.java
+++ src/main/java/oop/berc_oop_project/Nishe_2321153/UserManagement.java
@@ -4,4 +4,32 @@ public class UserManagement {
     private String username;
     private String userrole;
 
+    public UserManagement(String username, String userrole) {
+        this.username = username;
+        this.userrole = userrole;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserrole() {
+        return userrole;
+    }
+
+    public void setUserrole(String userrole) {
+        this.userrole = userrole;
+    }
+
+    @Override
+    public String toString() {
+        return "UserManagement{" +
+                "username='" + username + '\'' +
+                ", userrole='" + userrole + '\'' +
+                '}';
+    }
 }
