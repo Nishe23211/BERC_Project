46cad2eceb288a51a081c776c4834ec404734ade
Sayma <saymaakter4623@gmail.com>
2024-12-13 21:35:55 +0600

    complete policy maker goal 1

--- src/main/java/oop/berc_oop_project/Sayma_2320543/PolicyMaker1.java
+++ src/main/java/oop/berc_oop_project/Sayma_2320543/PolicyMaker1.java
@@ -1,22 +1,30 @@
 package oop.berc_oop_project.Sayma_2320543;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class PolicyMaker1 {
+
+
     @javafx.fxml.FXML
-    private Label DisplayText;
+    private Label SetText;
     @javafx.fxml.FXML
     private TextField passwordField;
     @javafx.fxml.FXML
-    private TextField governmentidField;
-    @javafx.fxml.FXML
     private ComboBox<String> typeofcredentialComboBox;
+    @javafx.fxml.FXML
+    private TextField governmentidField;
+
+    @FXML
+    public void initialize() {
+        typeofcredentialComboBox.getItems().addAll("Credential1","Credential2","Credential3");
+    }
 
     @javafx.fxml.FXML
     public void onLogInButtonClick(ActionEvent actionEvent) {
+        SetText.setText("show and use services");
     }
-
 }
